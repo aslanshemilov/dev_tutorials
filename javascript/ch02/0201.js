372928303dc5654573b028be6d5e978c9d9ca5d2
Shilpa Kancharla <shilpa.kancharla@verasci.com>
2019-07-10 09:58:59 -0400

    finished variable and types chapter

---
+++ javascript/ch02/0201.js
@@ -0,0 +1,52 @@
+//Assigning value to variable. Use this in the developer tools --> console.
+x = 32; //end every line with a semicolon
+32
+x;
+whereAmI = "Santa Barbara, CA";
+whereAmI = 75;
+var monster1 = "Grover", monster2 = "Cookie Monster", monster3 = "Big Bird";
+
+//Use camel case
+//Be wary of reserved words: var, function - VSCode highlights them in purple
+
+//Strings
+"12";
+"This is a string"; //use double quotes
+'This is also a string' //use single quotes
+//You can use either, just be consistent
+
+"Favorite \"string\" ";
+
+"This is \
+my new \
+line in the \
+string."
+
+var myString = "This is my string."
+myString.length;
+myString.toUpperCase;
+myString.toLowerCase;
+
+Infinity;
+-Infinity;
+NaN;
+
+var myNumber = 32; //can also use dot operator here
+
+Math.E;
+Math.round(1.1374983401);
+Math.round(1.89980983092);
+Math.random();
+
+//Booleans
+true;
+false; //only lowercase
+
+//Set variables to true or false
+buttonHasBeenClicked = false;
+
+var myLocation = "Santa Barbara", myOtherLocation = "Los Angeles";
+myLocation === myOtherLocation; //Should return false
+
+myOtherLocation = "Santa Barbara";
+myLocation === myOtherLocation; //Should return true
